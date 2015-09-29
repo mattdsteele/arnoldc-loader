8a8365dc1f553605c772e3eaf34550330a3abd46
Matt Steele <orphum@gmail.com>
2015-09-28 23:12:52 -0500

    Mark as cacheable

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var transpile = require('arnoldc.js/lib/Transpiler').transpile;
 var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
+  this.cacheable();
+
   var fileName =  loaderUtils.getRemainingRequest(this);
   var mapping = transpile(source, fileName);
   var output = mapping.toStringWithSourceMap({ file: fileName });
+
   this.callback(null, output.code, output.map.toString());
 };
